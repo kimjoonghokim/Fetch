622e19b1ae4aa024d82aa5ed8bfedfa0bd26806c
xrhuang10 <xrhuang10@gmail.com>
2025-07-28 17:59:19 -0400

    requirements

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
 pip install -r requirements.txt
 
 python3 -m vllm.entrypoints.openai.api_server --model xmu-nlp/Llama-3-8b-gsm8k --port
