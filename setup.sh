fa2bbcc4d0bccea4c09860765a02071bd29dbb6f
Joongho Kim <39285106+kimjoonghokim@users.noreply.github.com>
2025-09-01 18:52:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-export NCCL_P2P_DISABLE=1
+export NCCL_P2P_DISABLE=1 #disabling this helps with our NCCL issues on vLLM, feel free to comment this out if not necessary
